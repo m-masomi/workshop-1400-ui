bca3b75cab9300fd684462678e15f358c07eded7
mmasomi <mmasomi73@gmail.com>
2022-02-12 14:18:42 +0330

    feat (Login): Add Login Page

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  content: ["./Pages/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        white: "#ffffff",
+        black: "#000",
+        border: {
+          default: "#485164",
+        },
+        'river': {
+          default: '#485164',
+          '50': '#A5ADBE',
+          '100': '#99A3B6',
+          '200': '#828DA5',
+          '300': '#6A7793',
+          '400': '#59647C',
+          '500': '#485164',
+          '600': '#313743',
+          '700': '#191C23',
+          '800': '#020202',
+          '900': '#000000'
+        },
+      },
+      fontSize: {
+        12: ["12px", { lineHeight: "20px" }],
+        14: ["14px", { lineHeight: "24px" }],
+        16: ["16px", { lineHeight: "24px" }],
+        18: ["18px", { lineHeight: "25px" }],
+        20: ["20px", { lineHeight: "25px" }],
+        26: ["26px", { lineHeight: "32px" }],
+        30: ["30px", { lineHeight: "35px" }],
+        96: ["96px", { lineHeight: "120px" }],
+      },
+      fontFamily: {
+        sans: ["Vazir", "ui-sans-serif", "system-ui"],
+        serif: ["Vazir", "ui-serif", "Georgia"],
+        mono: ["Vazir", "ui-monospace", "SFMono-Regular"],
+        display: ["Vazir", "Oswald"],
+        body: ["Vazir", "Open Sans"],
+      },
+    },
+  },
+  plugins: [],
+};
